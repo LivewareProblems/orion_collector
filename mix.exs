fb95a3a2e1f86181e31a3dd77173ae731ad8b6cb
Thomas Depierre <depierre.thomas@gmail.com>
2021-11-04 15:33:37 +0100

    add tracer

--- mix.exs
+++ mix.exs
@@ -22,6 +22,9 @@ defmodule OrionCollector.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dog_sketch, "~> 0.1.2"},
+      {:ex2ms, "~> 1.6"}
+
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
